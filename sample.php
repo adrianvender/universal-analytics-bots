0926764435a27116a6fba941ff13ad4f8c0f85ca
adrianvender <adrianvender@gmail.com>
2016-01-17 11:37:30 -0700

    Create sample.php
    Sample code to place inside your PHP template.

---
+++ sample.php
@@ -0,0 +1,15 @@
+<?php
+/**********UA For Search Bots**************/
+
+//Configuration
+$UA_SB_ACCOUNT_ID = "UA-XXXXXX-YY"; //Replace with the UA Web Property ID.
+$UA_SB_PATH = "ua-searchbots/ua-searchbots.php"; //location of the UA for Search Bots script
+
+
+//Do not edit below this line
+
+//Execute the UA for Search Bots script
+include($UA_SB_PATH);
+
+/**********UA For Search Bots**************/
+?>
